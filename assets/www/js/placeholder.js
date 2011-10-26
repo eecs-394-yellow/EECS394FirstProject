d0e53581d270439cf30cf396eedd42c14a34666c
Ethan Romba <EthanRomba@u.northwestern.edu>
2011-10-26 01:26:22 -0500

    Cleaned up placeholder.js formatting

--- assets/www/js/placeholder.js
+++ assets/www/js/placeholder.js
@@ -1,22 +1,38 @@
-$('[placeholder]').focus(function() {
-  var input = $(this);
-  if (input.val() == input.attr('placeholder')) {
-    input.val('');
-    input.removeClass('placeholder');
-  }
-}).blur(function() {
-  var input = $(this);
-  if (input.val() == '' || input.val() == input.attr('placeholder')) {
-    input.addClass('placeholder');
-    input.val(input.attr('placeholder'));
-  }
-}).blur();
+/**
+ * Cross-Browser HTML5 Placeholder Text
+ * 
+ * Inspired by Nico Hagenburger:
+ * http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+ */
 
-$('[placeholder]').parents('form').submit(function() {
-  $(this).find('[placeholder]').each(function() {
-    var input = $(this);
-    if (input.val() == input.attr('placeholder')) {
-      input.val('');
+$('[placeholder]')
+  .focus(function() {
+    var $input = $(this);
+    if ($input.val() == $input.attr('placeholder')) {
+      $input
+        .val('')
+        .removeClass('placeholder');
     }
   })
-});
+  .blur(function() {
+    var $input = $(this);
+    if ($input.val() == '' || $input.val() == $input.attr('placeholder')) {
+      $input
+        .addClass('placeholder')
+        .val( $input.attr('placeholder') );
+    }
+  })
+  .blur();
+
+// Ensure the placeholder text is never submitted to the server
+
+$('[placeholder]')
+  .parents('form')
+    .submit(function() {
+      $(this).find('[placeholder]').each(function() {
+        var $input = $(this);
+        if ($input.val() == $input.attr('placeholder')) {
+          $input.val('');
+        }
+      })
+    });
